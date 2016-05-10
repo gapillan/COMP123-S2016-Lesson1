6627c51b1a9f72d4a7c7111305157e78c0938448
gapillan <gapillan@my.centennialcollege.ca>
2016-05-10 14:18:42 -0400

    Commit

--- COMP123-S2016-Lesson1/Program.cs
+++ COMP123-S2016-Lesson1/Program.cs
@@ -10,6 +10,17 @@ namespace COMP123_S2016_Lesson1
     {
         static void Main(string[] args)
         {
+            //Call the Hellow world to Console Method 
+            HelloWorldToConsole();
+        }
+
+       
+        public static string HelloWorldToConsole() 
+        { 
+          //* @description This is my Hello World Method @method HelloWorldToConsole @return (string) HelloWorldString //
+            string HelloWorldString = "Hello, World!";
+            Console.WriteLine(HelloWorldString);
+            return HelloWorldString; 
         }
     }
 }
